8eb376fe085fad2f39f5131147968e87d907b61b
Bowen Li <roamsir@gmail.com>
2025-01-01 04:33:39 +0800

    Bowen test2

--- test1/main.cpp
+++ test1/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "test1.h"
+#include "test2.h"
 
 using namespace std;
 
@@ -8,6 +9,8 @@ int main(int argc, char* argv[])
 	cout << "hello,James,go gogo!" << endl;
 
 	test1();
+
+	test22();
 
 	return 0;
 }
---
+++ test1/test2.cpp
@@ -0,0 +1,8 @@
+#include "test2.h"
+#include <iostream>
+using namespace std;
+
+void test22()
+{
+	cout << "test22,Bowen!" << endl;
+}
